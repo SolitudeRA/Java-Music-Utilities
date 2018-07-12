59363e672ea1b3eb488044752e2b7bab228bbf0f
Arthur.Lee <arthur.lee.ac72@gmail.com>
2018-07-12 22:18:04 +0800

    Update from earth, metadata complete, getCover needed

--- src/main/java/org/protogalaxy/multimedia/MusicContainer.java
+++ src/main/java/org/protogalaxy/multimedia/MusicContainer.java
@@ -11,9 +11,11 @@ import static org.bytedeco.javacpp.avutil.*;
 import static org.bytedeco.javacpp.swscale.*;
 
 public class MusicContainer {
+    private Path path = null;
     private AVFormatContext avFormatContext = avformat_alloc_context();
 
     public MusicContainer(Path path) {
+        this.path = path;
         av_register_all();
         avformat_open_input(avFormatContext, path.toString(), null, null);
         avformat_find_stream_info(avFormatContext, (PointerPointer) null);
@@ -23,6 +25,14 @@ public class MusicContainer {
         BytePointer bytePointer = new BytePointer(stream);
         av_register_all();
         //TODO: stream reader from memory
+    }
+
+    public void destory() {
+        avformat_close_input(avFormatContext);
+    }
+
+    public Path getPath() {
+        return path;
     }
 
     public AVFormatContext getAvFormatContext() {
--- src/main/java/org/protogalaxy/multimedia/components/Metadata.java
+++ src/main/java/org/protogalaxy/multimedia/components/Metadata.java
@@ -1,7 +1,19 @@
 package org.protogalaxy.multimedia.components;
 
+import static org.bytedeco.javacpp.avcodec.*;
+import static org.bytedeco.javacpp.avformat.*;
+import static org.bytedeco.javacpp.avutil.*;
+import static org.bytedeco.javacpp.swscale.*;
+
+import org.protogalaxy.multimedia.MusicContainer;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
+//TODO: Cover
 public class Metadata {
     private String title;
     private String album;
@@ -15,24 +27,219 @@ public class Metadata {
     private String genre;
     private String publisher;
     private String comment;
-    private String duration_milliseconds;
-    private String duration_seconds;
+    private long duration_milliseconds;
+    private long duration_seconds;
     private String duration_formatted;
-    private int bitrate_raw;
+    private long bitrate_raw;
     private String bitrate_formatted;
+    private int sampleRate_raw;
+    private String sampleRate_formatted;
     private int bitDepth_raw;
     private String bitDepth_formatted;
-    private int size_kilobytes_raw;
+    private long size_kilobytes_raw;
     private String size_kilobytes_formatted;
-    private int size_megabytes_raw;
+    private double size_megabytes_raw;
     private String size_megabytes_formatted;
 
-    public void read() {
+    private String
+            METADATA_TITLE = "title",
+            METADATA_ALBUM = "album",
+            METADATA_ARTIST = "artist",
+            METADATA_ALBUMARTIST = "album_artist",
+            METADATA_COMPOSER = "composer",
+            METADATA_PERFORMER = "performer",
+            METADATA_DATE = "date",
+            METADATA_TRACK = "track",
+            METADATA_DISC = "disc",
+            METADATA_GENRE = "genre",
+            METADATA_PUBLISHER = "publisher",
+            METADATA_COMMENT = "comment",
+            METADATA_DURATION = "duration",
+            METADATA_BITRATE = "bitrate",
+            METADATA_SAMPLERATE = "sample_rate",
+            METADATA_BITDEPTH = "bit_depth",
+            METADATA_SIZE = "size",
+            METADATA_COVER = "cover";
+
+    private ArrayList<String> metadataStandardArray = new ArrayList<>(Arrays.asList(
+            METADATA_TITLE,
+            METADATA_ALBUM,
+            METADATA_ARTIST,
+            METADATA_ALBUMARTIST,
+            METADATA_COMPOSER,
+            METADATA_PERFORMER,
+            METADATA_DATE,
+            METADATA_TRACK,
+            METADATA_DISC,
+            METADATA_GENRE,
+            METADATA_PUBLISHER,
+            METADATA_COMMENT
+    ));
+    private ArrayList<String> metadataFullArray = new ArrayList<>(Arrays.asList(
+            METADATA_TITLE,
+            METADATA_ALBUM,
+            METADATA_ARTIST,
+            METADATA_ALBUMARTIST,
+            METADATA_COMPOSER,
+            METADATA_PERFORMER,
+            METADATA_DATE,
+            METADATA_TRACK,
+            METADATA_DISC,
+            METADATA_GENRE,
+            METADATA_PUBLISHER,
+            METADATA_COMMENT,
+            METADATA_DURATION,
+            METADATA_BITRATE,
+            METADATA_SAMPLERATE,
+            METADATA_BITDEPTH,
+            METADATA_SIZE,
+            METADATA_COVER
+    ));
+    private Map<String, String> metadataStandardMap = new HashMap<>();
+    private Map<String, String> metadataFullMap = new HashMap<>();
+
+    /**
+     * Initialize the metadata object
+     *
+     * @param container music file container
+     */
+    public Metadata(MusicContainer container) {
+        AVDictionaryEntry entry = null;
+        Map<String, String> metadataCacheMap = new HashMap<>();
+        while ((entry = av_dict_get(container.getAvFormatContext().metadata(), "", entry, AV_DICT_IGNORE_SUFFIX)) != null) {
+            metadataCacheMap.put(entry.key().getString(), entry.value().getString());
+        }
+        for (String metadata : metadataStandardArray) {
+            metadataStandardMap.put(metadata, metadataCacheMap.get(metadata));
+        }
+        for (String metadata : metadataStandardArray) {
+            metadataFullMap.put(metadata, metadataCacheMap.get(metadata));
+        }
+        metadataFullMap.put(METADATA_DURATION, formatDuration(container.getAvFormatContext().duration()));
+        metadataFullMap.put(METADATA_BITRATE, formatBitrate(container.getAvFormatContext().streams(0).codecpar().bit_rate()));
+        metadataFullMap.put(METADATA_SAMPLERATE, formatSampleRate(container.getAvFormatContext().streams(0).codecpar().sample_rate()));
+        metadataFullMap.put(METADATA_BITDEPTH, formatBitDepth(container.getAvFormatContext().streams(0).codecpar().bits_per_raw_sample()));
+        metadataFullMap.put(METADATA_SIZE, formatSizeMegabytes(container.getPath().toFile().length()));
+        this.duration_milliseconds = 1000 * (container.getAvFormatContext().duration() / AV_TIME_BASE) + (1000 * (container.getAvFormatContext().duration() % AV_TIME_BASE)) / AV_TIME_BASE;
+        this.duration_seconds = (container.getAvFormatContext().duration() / AV_TIME_BASE);
+        this.duration_formatted = formatDuration(container.getAvFormatContext().duration());
+        this.bitrate_raw = container.getAvFormatContext().streams(0).codecpar().bit_rate();
+        this.bitrate_formatted = formatBitrate(container.getAvFormatContext().streams(0).codecpar().bit_rate());
+        this.sampleRate_raw = container.getAvFormatContext().streams(0).codecpar().sample_rate();
+        this.sampleRate_formatted = formatSampleRate(container.getAvFormatContext().streams(0).codecpar().sample_rate());
+        this.bitDepth_raw = container.getAvFormatContext().streams(0).codecpar().bits_per_raw_sample();
+        this.bitDepth_formatted = formatBitDepth(container.getAvFormatContext().streams(0).codecpar().bits_per_raw_sample());
+        this.size_kilobytes_raw = container.getPath().toFile().length() / 1024;
+        this.size_kilobytes_formatted = formatSizeKilobytes(container.getPath().toFile().length());
+        this.size_megabytes_raw = container.getPath().toFile().length() / 1024 / 1024;
+        this.size_megabytes_formatted = formatSizeMegabytes(container.getPath().toFile().length());
+    }
+
+    public void write(MusicContainer container) {
 
     }
 
-    public void write() {
+    /**
+     * Format track duration
+     *
+     * @param duration duration of the track
+     * @return formatted duration of the track
+     */
+    private String formatDuration(long duration) {
+        long secs, us;
+        secs = (duration / AV_TIME_BASE);
+        us = (1000 * (duration % AV_TIME_BASE)) / AV_TIME_BASE;
+        long totalus = 1000 * secs + us;
+        SimpleDateFormat format = new SimpleDateFormat("mm:ss:SSS");
+        return format.format(totalus);
+    }
 
+    /**
+     * Format track bitrate
+     *
+     * @param bitrate bitrate of the track
+     * @return formatted bitrate
+     */
+    private String formatBitrate(long bitrate) {
+        return String.valueOf(bitrate / 1000) + "kbps";
+    }
+
+    /**
+     * Format track sample rate
+     *
+     * @param sampleRate sample rate of the track
+     * @return formatted sample rate
+     */
+    private String formatSampleRate(int sampleRate) {
+        return String.valueOf(sampleRate) + "Hz";
+    }
+
+    /**
+     * Format track bit depth
+     *
+     * @param bitDepth bit depth of the track
+     * @return formatted bit depth
+     */
+    private String formatBitDepth(int bitDepth) {
+        return String.valueOf(bitDepth) + "Bit";
+    }
+
+    /**
+     * Format size of the file in kilobyte
+     *
+     * @param size size of the file
+     * @return formatted size of the file
+     */
+    private String formatSizeKilobytes(long size) {
+        size = size / 1024;
+        return String.valueOf(size) + "KB";
+    }
+
+    /**
+     * Format size of the file in megabyte
+     *
+     * @param size size of the file
+     * @return formatted size of the file
+     */
+    private String formatSizeMegabytes(long size) {
+        size = size / 1024 / 1024 * 100;
+        return String.valueOf((size / 100)) + "." + String.valueOf((size % 100)) + "MB";
+    }
+
+    /**
+     * Get standard metadata name list
+     *
+     * @return string list of the standard metadata name
+     */
+    public ArrayList<String> getMetadataStandardArray() {
+        return metadataStandardArray;
+    }
+
+    /**
+     * Get all metadata name list
+     *
+     * @return string list of all metadata name
+     */
+    public ArrayList<String> getMetadataFullArray() {
+        return metadataFullArray;
+    }
+
+    /**
+     * Get standard metadata map
+     *
+     * @return map of the standard metadata
+     */
+    public Map<String, String> getMetadataStandardMap() {
+        return metadataStandardMap;
+    }
+
+    /**
+     * Get full metadata map
+     *
+     * @return map of all metadata
+     */
+    public Map<String, String> getMetadataFullMap() {
+        return metadataFullMap;
     }
 
     public String getTitle() {
@@ -131,11 +338,11 @@ public class Metadata {
         this.comment = comment;
     }
 
-    public String getDuration_milliseconds() {
+    public long getDuration_milliseconds() {
         return duration_milliseconds;
     }
 
-    public String getDuration_seconds() {
+    public long getDuration_seconds() {
         return duration_seconds;
     }
 
@@ -143,12 +350,20 @@ public class Metadata {
         return duration_formatted;
     }
 
-    public int getBitrate_raw() {
+    public long getBitrate_raw() {
         return bitrate_raw;
     }
 
     public String getBitrate_formatted() {
         return bitrate_formatted;
+    }
+
+    public int getSampleRate_raw() {
+        return sampleRate_raw;
+    }
+
+    public String getSampleRate_formatted() {
+        return sampleRate_formatted;
     }
 
     public int getBitDepth_raw() {
@@ -159,7 +374,7 @@ public class Metadata {
         return bitDepth_formatted;
     }
 
-    public int getSize_kilobytes_raw() {
+    public long getSize_kilobytes_raw() {
         return size_kilobytes_raw;
     }
 
@@ -167,7 +382,7 @@ public class Metadata {
         return size_kilobytes_formatted;
     }
 
-    public int getSize_megabytes_raw() {
+    public double getSize_megabytes_raw() {
         return size_megabytes_raw;
     }
 
